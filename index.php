502a2b464cf6d7525512fd75d08ad46a869cf12c
DiptaDev <111142705+DiptaDev@users.noreply.github.com>
2022-08-14 22:58:50 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css"> 
+    <!-- set ur css file 👆 -->
+    <title>Lifecraft - home</title>
+</head>
+<body>
+    <p>Button Tample👇</p>
+        
+
+    <a href="#setur Link here" class="bubbly-button">Ur text</a>
+    <!-- the button👆 -->
+</body>
+<script src="index.js"></script>  
+<!-- set ur .js file here👆 -->
+</html>
